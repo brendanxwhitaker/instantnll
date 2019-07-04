3dbb1c1df379a74f35dafc782d938895f79640ea
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-07-03 21:26:27 -0400

    Passes first nontrivial testcase.

--- instantnll/tests/simrel_test.py
+++ instantnll/tests/simrel_test.py
@@ -1,5 +1,6 @@
 # pylint: disable=no-self-use,invalid-name,protected-access
 from numpy.testing import assert_almost_equal
+from scipy.spatial.distance import cosine
 import pytest
 import torch
 
@@ -17,8 +18,8 @@ class TestSimRel(AllenNlpTestCase):
                 'num_classes': 3,
                 })
         simrel = SimRel.from_params(params)
-        assert len(simrel.get_output_dim) == 3
-        assert len(simrel.get_input_dim) == 2
+        assert simrel.get_output_dim() == 3
+        assert simrel.get_input_dim() == 2
 
     def test_forward_gives_correct_output(self):
         params = Params({
@@ -34,7 +35,10 @@ class TestSimRel(AllenNlpTestCase):
         input_tensor = torch.FloatTensor([[-3, 1]])
         avg_tensor_list = [torch.FloatTensor([5, 5])]
         output = simrel(input_tensor, avg_tensor_list).data.numpy()
-        assert output.shape == (1, 3)
+        print("===OUTPUT===")
+        print(output)
+        print("============")
+        assert output.shape == (1, 1)
         # This output was checked by hand - ReLU makes output after first hidden layer [0, 0, 0],
         # which then gets a bias added in the second layer to be [1, 1, 1].
-        assert_almost_equal(output, [[1, 1, 1]])
+        assert_almost_equal(output, torch.FloatTensor([[-0.44721356]])) 
