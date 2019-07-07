641a2237e7ff2614f63e48557b55b9a282fd9fc4
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-07-07 11:22:20 -0400

    vocab debug file.

---
+++ subset.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+    m = int(sys.argv[2])
+    with open(path, 'r') as the_file:
+        lines = the_file.readlines()
+    n = len(lines)
+    assert m <= n
+    with open(path + "_TRUNC_" + str(m), 'a') as new_file:
+        for line in lines[:m]:
+            new_file.write(line + '\n')
