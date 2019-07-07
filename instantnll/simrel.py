bdf912da24df81a3fae421b32f77e01c742cc48f
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-07-07 08:23:20 -0400

    Linted.

--- instantnll/simrel.py
+++ instantnll/simrel.py
@@ -1,21 +1,17 @@
 """
-A similarity/relatedness computation against a fixed number of class avg vectors. 
+A similarity/relatedness computation against a fixed number of class avg vectors.
 """
-from typing import List, Union
-from numpy.testing import assert_almost_equal
+from typing import List
 
-import copy
 import torch
 
 from allennlp.common import FromParams
-from allennlp.common.checks import ConfigurationError
-from allennlp.nn import Activation
 from allennlp.modules.similarity_functions import CosineSimilarity
 
 class SimRel(torch.nn.Module, FromParams):
     """
     This ``Module`` applies a simple similarity/relatedness computation to each input vector
-    against each of a fixed number of class avg vectors. 
+    against each of a fixed number of class avg vectors.
 
     Parameters
     ----------
@@ -24,7 +20,7 @@ class SimRel(torch.nn.Module, FromParams):
     class_avgs : ``List[torch.LongTensor]``
         The average of the embedding vectors for each seen class. Has shape (num_classes,).
     """
-    def __init__(self, input_dim: int, 
+    def __init__(self, input_dim: int,
                  num_classes: int) -> None:
 
         super(SimRel, self).__init__()
@@ -38,15 +34,15 @@ class SimRel(torch.nn.Module, FromParams):
     def get_input_dim(self):
         return self.input_dim
 
-    def forward(self, 
-                inputs: torch.Tensor, # Shape: (batch_size, #_tokens_in_longest_inst, input_dim) 
-                labels: torch.Tensor, # Shape: (batch_size, #_tokens_in_longest_inst) 
+    def forward(self,
+                inputs: torch.Tensor, # Shape: (batch_size, #_tokens_in_longest_inst, input_dim)
+                labels: torch.Tensor, # Shape: (batch_size, #_tokens_in_longest_inst)
                 class_avgs: List[torch.LongTensor]) -> torch.Tensor:
         # pylint: disable=arguments-differ
-        if self.debug: 
+        if self.debug:
             print("===SIMREL DEBUG===")
-            print("Are we training:", self.training) 
-            print("===SIMREL DEBUG===") 
+            print("Are we training:", self.training)
+            print("===SIMREL DEBUG===")
         if self._output_dim != len(class_avgs):
             print("===================")
             print("Parameter `num_classes` in config doesn't match the total number of classes \
@@ -56,48 +52,47 @@ class SimRel(torch.nn.Module, FromParams):
             print("===================")
             assert self._output_dim == len(class_avgs)
         cosine_similarity = CosineSimilarity()
-   
+
         """
-        Computes the cosine similarity of each word vector with every vector in class_avgs. 
+        Computes the cosine similarity of each word vector with every vector in class_avgs.
 
         Perhaps it doesn't make much sense to train on the trivial class. Yes it doesn't make sense
         to train on the trivial class, because then, in the case where the token currently being tagged
-        is farther from the trivial class avg vector than it is from the nontrivial class avg vectors, 
+        is farther from the trivial class avg vector than it is from the nontrivial class avg vectors,
         it will label it as a nontrivial entity, even if it's really far away from everything. This is
-        bad behavior. So maybe a SimRel threshold parameter is a better approach.  
-        """ 
-  
-        
+        bad behavior. So maybe a SimRel threshold parameter is a better approach.
+        """
+
         class_counts = [0] * len(class_avgs)
- 
+
         output = []
         for i, batch in enumerate(inputs):
             batch_out = []
             for j, vec in enumerate(batch):
-                
+
                 simvals = []
                 for k, class_vec in enumerate(class_avgs):
-                    
-                    # If we haven't initialized a class vector. 
+
+                    # If we haven't initialized a class vector.
                     if True in torch.isinf(class_vec):
-                        
+
                         # If we need to initialize a new class vector.
                         if labels is not None and labels[i][j] == k:
-                            simvals.append(cosine_similarity(vec,vec)) 
+                            simvals.append(cosine_similarity(vec, vec))
                             if self.training:
                                 class_avgs[k] = vec
-                                class_counts[k] += 1 
-                        
-                        # Otherwise, we set the similarity value to -1. 
+                                class_counts[k] += 1
+
+                        # Otherwise, we set the similarity value to -1.
                         else:
-                            simvals.append(torch.tensor(-1.0)) 
-                    
+                            simvals.append(torch.Tensor(-1.0))
+
                     else:
                         simvals.append(cosine_similarity(vec, class_vec))
                         if self.training and labels[i][j] == k:
                             class_vec_multiple = class_vec * class_counts[k]
                             class_avgs[k] = (class_vec_multiple + vec) / (class_counts[k] + 1)
-                            class_counts[k] += 1 
+                            class_counts[k] += 1
                 if self.debug:
                     self._print_class_avgs(class_avgs)
 
@@ -109,6 +104,6 @@ class SimRel(torch.nn.Module, FromParams):
     def _print_class_avgs(self, class_avgs: List[torch.Tensor]) -> None:
 
         for class_avg in class_avgs:
-            # print(hex(id(class_avg)), end = ' ')
-            print(float(class_avg[0]), end = ' ')
+            # print(hex(id(class_avg)), end=' ')
+            print(float(class_avg[0]), end=' ')
         print("")
